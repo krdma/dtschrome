6be38a0ce5ea953eb7cd103553ac5d25d656a731
krdma <krdma@mail.ru>
2025-07-31 03:15:53 +0300

    Modify Hotelston autofill

--- autofill-extension/hotelston.js
+++ autofill-extension/hotelston.js
@@ -4,15 +4,23 @@
   function fillHotelston(data) {
     const pax = data && data.passports ? data.passports : passengers;
     const firstInputs = document.querySelectorAll("input[name='firstname']");
-    firstInputs.forEach((el, idx) => {
-      const first = pax[idx] ? (pax[idx].first_name || pax[idx].firstName) : (pax[0].first_name || pax[0].firstName);
-      setValue(el, first);
-    });
+    Array.from(firstInputs)
+      .slice(0, pax.length)
+      .forEach((el, idx) => {
+        const first = pax[idx]
+          ? pax[idx].first_name || pax[idx].firstName
+          : pax[0].first_name || pax[0].firstName;
+        setValue(el, first);
+      });
     const lastInputs = document.querySelectorAll("input[name='lastname']");
-    lastInputs.forEach((el, idx) => {
-      const last = pax[idx] ? (pax[idx].last_name || pax[idx].lastName) : (pax[0].last_name || pax[0].lastName);
-      setValue(el, last);
-    });
+    Array.from(lastInputs)
+      .slice(0, pax.length)
+      .forEach((el, idx) => {
+        const last = pax[idx]
+          ? pax[idx].last_name || pax[idx].lastName
+          : pax[0].last_name || pax[0].lastName;
+        setValue(el, last);
+      });
   }
 
   if (document.readyState === 'loading') {
